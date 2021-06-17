e9d09d0effa149fc26520d19fd20c48407ba70f1
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2021-06-17 16:14:59 -0300

    README atualizado e algumas classes modificadas

--- src/src/game/city/institution/InstitutionControl.java
+++ src/src/game/city/institution/InstitutionControl.java
@@ -1,4 +1,21 @@
 package game.city.institution;
 
+import game.city.IUpdateParameters;
+import game.gameControl.IPauseTimer;
+import game.view.IUpdateBar;
+import game.view.institutionview.IUpdateView;
+
 public class InstitutionControl {
+    IPauseTimer timerControl;
+    IUpdateBar barControl;
+    IUpdateView viewUpdate;
+    IUpdateParameters cityParameters;
+
+    public void connect(IUpdateView viewUpdate){
+        this.viewUpdate = viewUpdate;
+    }
+
+    public void connect(IUpdateParameters cityParameters){
+        this.cityParameters = cityParameters;
+    }
 }
--- src/src/game/gameBuilder/Builder.java
+++ src/src/game/gameBuilder/Builder.java
@@ -1,4 +1,50 @@
 package game.gameBuilder;
 
-public class Builder {
+import game.city.*;
+import game.city.institution.*;
+import game.gameControl.IConnectComponents;
+import game.gameControl.IPauseTimer;
+import game.mayor.*;
+import game.view.*;
+import game.view.institutionview.*;
+
+public class Builder implements IRConnectComponents {
+    char[][] matrix;
+    City city;
+    View view;
+    Mayor mayor;
+    IConnectComponents game;
+    int sizeX;
+    int sizeY;
+
+    public Builder(){
+        matrix = new char[][]{ {'P', '-','-','H','-','-','-','B'},
+                               {'-', '-','-','-','-','-','-','-'},
+                               {'-', '-','S','-','-','-','-','-'},
+                               {'I', '-','-','-','C','C','C','C'}};
+        sizeX = 8;
+        sizeY = 4;
+    }
+
+    public void buildGame(IPauseTimer timerControl){
+        city = new City();
+        view = new View();
+        mayor = new Mayor();
+        city.buildMatrix(sizeX, sizeY);
+        for(int y = 0; y < sizeY; y++){
+            for(int x = 0; x < sizeX; x++){
+                if(matrix[y][x] != '-'){
+                    InstitutionControl institControl = city.insert(x, y, matrix[y][x]);
+                    InstitutionView institView = view.insert(x, y, matrix[y][x]);
+                    institControl.connect((IUpdateParameters) city);
+                    institControl.connect((IUpdateView) institView);
+                }
+            }
+        }
+    }
+
+    public void connect(IConnectComponents game){
+        this.game = game;
+    }
+
 }
